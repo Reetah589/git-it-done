2341a89861b2f802f6e23e8071f70f4f24f461fe
reetah589 <reetah589@gmail.com>
2021-04-20 20:39:29 -0500

    octo updated

--- homepage.js
+++ homepage.js
@@ -1,5 +1,21 @@
 var getUserRepos = function() {
   fetch("https://api.github.com/users/octocat/repos");
+
+  var response = fetch("https://api.github.com/users/octocat/repos");
+  console.log(response);
+
+  fetch("https://api.github.com/users/octocat/repos").then(function(response) {
+  console.log("inside", response);
+  });
+
+  console.log("outside");
+
+  fetch("https://api.github.com/users/octocat/repos").then(function(response) {
+  response.json().then(function(data) {
+    console.log(data);
+  });
+ });
+
 };
   
   getUserRepos();
